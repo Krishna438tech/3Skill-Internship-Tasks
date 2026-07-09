2d7dc62a7fe1a053da9b4dae70e016398e428b21
Krishna438tech <k6034760@gmail.com>
2026-07-09 18:42:23 +0530

    Connect frontend with deployed backend

--- Task-4_QuizSite/frontend/src/services/api.js
+++ Task-4_QuizSite/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://quizsite-backend-8bnj.onrender.com/api",
 });
 
 // Attach JWT automatically
